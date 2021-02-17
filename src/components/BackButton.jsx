2d3cdb7cf305936c6dea63bf2fb4ca8b73f3e469
tmccoy529 <40430541+tmccoy529@users.noreply.github.com>
2021-02-17 08:14:53 -0500

    pulled css out into component library

--- src/components/BackButton.jsx
+++ src/components/BackButton.jsx
@@ -3,8 +3,8 @@ import React from "react";
 const BackButton = (props) => {
   if (props.currentStep !== 1)
     return (
-      <div onClick={props.previousStep} className="back-arrow">
-        <i className="fas fa-arrow-left"></i>
+      <div onClick={props.previousStep}>
+        <i className="back-arrow fas fa-arrow-left"></i>
       </div>
     );
   else return <div></div>;
